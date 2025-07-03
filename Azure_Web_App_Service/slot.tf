5ec199e9b650d38e1c8cf6ccd54be1d1b2d9c2e1
Władysław Lastowski <plankton.by@gmail.com>
2025-07-03 10:18:40 +0200

    Deployment slots swapped

--- Azure_Web_App_Service/slot.tf
+++ Azure_Web_App_Service/slot.tf
@@ -10,3 +10,7 @@ resource "azurerm_windows_web_app_slot" "webapp_slot" {
   }
   depends_on = [azurerm_service_plan.serviceplan]
 }
+
+resource "azurerm_web_app_active_slot" "activeslot" {
+  slot_id = azurerm_windows_web_app_slot.webapp_slot.id # Here we need to reference the slot to swap with Production, so we specify the staging slot created above
+}
